5cfc1678efe40d4d28801ba69ee1c805115fd6c1
Mohammad.Sj <mohammad.sj2@gmail.com>
2021-01-09 18:13:20 +0330

    + discount, airplane, seat and order table

--- create_table.py
+++ create_table.py
@@ -30,6 +30,7 @@ def create_employee(cursor):
             'code varchar(10), name varchar(20), jobType varchar(10),' \
             'employment_year varchar(4), salary real, totalSalary real,' \
             'primary key (code));'
+    # TODO employment_year -> employmentYear
     execute_create_table_query(cursor, query, 'EMPLOYEE')
 
 
@@ -51,3 +52,46 @@ def create_flight_crew(cursor):
             'foreign key (employeeCode) references EMPLOYEE(code),' \
             'foreign key (travelCode) references TRAVEL(code));'
     execute_create_table_query(cursor, query, 'FLIGHT_CREW')
+
+
+def create_discount(cursor):
+    query = '''CREATE TABLE DISCOUNT(
+            customerNC varchar(10), discountNo varchar(10), 
+            percent real, expirationTime timestamp,
+            orderNo varchar(10), customerOrderNC varchar (10),
+            primary key (customerNC, discountNo),
+            foreign key (customerNC) references CUSTOMER(NC),
+            foreign key (orderNo, customerOrderNC) references ORDER(orderNo, customerNC));'''
+    execute_create_table_query(cursor, query, 'DISCOUNT')
+
+
+def create_airplane(cursor):
+    query = '''CREATE TABLE AIRPLANE(
+                code varchar(10),
+                capacity integer,
+                model varchar(10),
+                city varchar(10),
+                primary key (code));'''
+    execute_create_table_query(cursor, query, 'AIRPLANE')
+
+
+def create_seat(cursor):
+    query = '''CREATE TABLE SEAT(
+                seatNo varchar(10),
+                airplaneCode varchar(10),
+                primary key (airplaneCode, seatNo),
+                foreign key (airplaneCode) references AIRPLANE(code));'''
+    execute_create_table_query(cursor, query, 'SEAT')
+
+
+def create_order(cursor):
+    query = '''CREATE TABLE ORDER(
+                customerNC varchar(10), orderNo varchar(10), 
+                paymentStatus varchar(10), travelCode timestamp,
+                score varchar(10), seatNo varchar(10),
+                airplaneCode varchar(10),
+                primary key (customerNC, orderNo),
+                foreign key (customerNC) references CUSTOMER(NC),
+                foreign key (travelCode) references TRAVEL(code),
+                foreign key (airplaneCode, seatNo) references SEAT(airplaneCode,seatNo));'''
+    execute_create_table_query(cursor, query, 'ORDER')
