fa0c56eff977ca002b9af23ff4585b92394c2db9
Keivan <k1rezaei78@gmail.com>
2021-01-09 20:12:41 +0330

    refactor

--- src/TableDefiner.py
+++ src/TableDefiner.py
@@ -10,46 +10,50 @@ def execute_create_table_query(cursor, query, table_name):
 
 
 def create_customer(cursor):
-    query = 'CREATE TABLE CUSTOMER(' \
-            'NC varchar(10),firstName varchar(15),' \
-            'lastName varchar(15), money real,' \
-            'primary key (NC));'
+    query = '''CREATE TABLE CUSTOMER(
+                NC varchar(10),firstName varchar(15),
+                lastName varchar(15), money real NOT NULL,
+                CHECK money >= 0,
+                primary key (NC));'''
     execute_create_table_query(cursor, query, 'CUSTOMER')
 
 
 def create_comment(cursor):
-    query = 'CREATE TABLE COMMENT(' \
-            'customerNC varchar(10),commentNO varchar(10),text varchar,' \
-            'primary key (customerNC, commentNo),' \
-            'foreign key (customerNC) references CUSTOMER(NC));'
+    query = '''CREATE TABLE COMMENT(
+                customerNC varchar(10),commentNO varchar(10),text varchar,
+                primary key (customerNC, commentNo),
+                foreign key (customerNC) references CUSTOMER(NC));'''
     execute_create_table_query(cursor, query, 'COMMENT')
 
 
 def create_employee(cursor):
-    query = 'CREATE TABLE EMPLOYEE(' \
-            'code varchar(10), name varchar(20), jobType varchar(10),' \
-            'employmentYear varchar(4), salary real, totalSalary real,' \
-            'primary key (code));'
+    query = '''CREATE TABLE EMPLOYEE(
+            code varchar(10), name varchar(20), jobType varchar(10),
+            employmentYear varchar(4), salary real NOT NULL, totalSalary real NOT NULL,
+            CHECK salary >= 0,
+            CHECK totalSalary >= 0,
+            primary key (code));'''
     execute_create_table_query(cursor, query, 'EMPLOYEE')
 
 
 def create_travel(cursor):
-    query = 'CREATE TABLE TRAVEL(' \
-            'code varchar(10), time timestamp, ' \
-            'startCity varchar(10), targetCity varchar(10), ticketPrice real, ' \
-            'airplaneCode varchar(10), captainCode varchar (10),' \
-            'primary key (code),' \
-            'foreign key(airplaneCode) references AIRPLANE(code),' \
-            'foreign key (captainCode) references EMPLOYEE(code));'
+    query = '''CREATE TABLE TRAVEL(
+                code varchar(10), time timestamp NOT NULL,
+                startCity varchar(10) NOT NULL, targetCity varchar(10) NOT NULL, ticketPrice real NOT NULL,
+                airplaneCode varchar(10) NOT NULL, captainCode varchar (10) NOT NULL,
+                CHECK ticketPrice >= 0,
+                primary key (code),
+                foreign key(airplaneCode) references AIRPLANE(code),
+                foreign key (captainCode) references EMPLOYEE(code));'''
     execute_create_table_query(cursor, query, 'TRAVEL')
 
 
 def create_flight_crew(cursor):
-    query = 'CREATE TABLE FLIGHT_CREW(' \
-            'employeeCode varchar(10), travelCode varchar(10),' \
-            'primary key(employeeCode, travelCode),' \
-            'foreign key (employeeCode) references EMPLOYEE(code),' \
-            'foreign key (travelCode) references TRAVEL(code));'
+    query = '''CREATE TABLE FLIGHT_CREW(
+                employeeCode varchar(10), travelCode varchar(10),
+                primary key(employeeCode, travelCode),
+                foreign key (employeeCode) references EMPLOYEE(code),
+                foreign key (travelCode) references TRAVEL(code));'''
     execute_create_table_query(cursor, query, 'FLIGHT_CREW')
 
 
